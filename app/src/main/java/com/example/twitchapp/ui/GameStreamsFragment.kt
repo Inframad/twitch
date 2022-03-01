aa66bdcdc29ca5c697982eb59eb901f4b49471a7
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-01 14:33:37 +0700

    Добавил верстку для списка стримов

---
+++ app/src/main/java/com/example/twitchapp/ui/GameStreamsFragment.kt
@@ -0,0 +1,28 @@
+package com.example.twitchapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.twitchapp.databinding.FragmentGameStreamsBinding
+
+class GameStreamsFragment : Fragment() {
+
+    private var _binding: FragmentGameStreamsBinding? = null
+    private val binding: FragmentGameStreamsBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGameStreamsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
