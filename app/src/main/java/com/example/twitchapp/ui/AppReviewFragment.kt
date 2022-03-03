e1f61e5b3c47588a184e3427b0595edfc1c9f401
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-03 19:01:59 +0700

    Установил слушатель на кнопку в AppReviewFragment, добавил landscape layout для item_game_stream

--- app/src/main/java/com/example/twitchapp/ui/AppReviewFragment.kt
+++ app/src/main/java/com/example/twitchapp/ui/AppReviewFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.twitchapp.R
 import com.example.twitchapp.databinding.FragmentAppReviewBinding
+import com.example.twitchapp.ui.util.showToast
 
 class AppReviewFragment : Fragment() {
 
@@ -19,6 +21,23 @@ class AppReviewFragment : Fragment() {
     ): View {
         _binding = FragmentAppReviewBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            sendReviewBtn.setOnClickListener {
+                context?.showToast(
+                    String.format(
+                        getString(R.string.review_is_sent_msg),
+                        appReviewEt.text,
+                        appRatingBar.rating.toString()
+                    )
+                )
+                parentFragmentManager.popBackStack()
+            }
+        }
     }
 
     override fun onDestroyView() {
