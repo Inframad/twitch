5550ed83564de809597dea6fc26c347ed3b59cee
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-10 10:34:44 +0700

    Добавил отображение списка избранных игр

--- app/src/main/java/com/example/twitchapp/ui/favourite/FavouriteGamesFragment.kt
+++ app/src/main/java/com/example/twitchapp/ui/favourite/FavouriteGamesFragment.kt
@@ -1,14 +1,84 @@
 package com.example.twitchapp.ui.favourite
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.twitchapp.R
+import com.example.twitchapp.data.model.game.Game
 import com.example.twitchapp.databinding.FragmentFavouriteGamesBinding
+import com.example.twitchapp.ui.UiState
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class FavouriteGamesFragment: Fragment(R.layout.fragment_favourite_games) {
+class FavouriteGamesFragment : Fragment(R.layout.fragment_favourite_games) {
 
     private val viewBinding: FragmentFavouriteGamesBinding by viewBinding()
+    private val viewModel: FavouriteGamesViewModel by viewModels()
 
+    private var adapter: FavouriteGamesAdapter? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        bindViewModel()
+    }
+
+    private fun initViews() {
+        adapter = FavouriteGamesAdapter()
+        viewBinding.recyclerView.adapter = adapter
+        viewBinding.noDataTextView.text = getString(R.string.no_saved_data_msg)
+    }
+
+    private fun bindViewModel() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { uiState ->
+                    when (uiState) {
+                        is UiState.Loaded -> showFavouriteGamesList(uiState.data)
+                        is UiState.Error -> showError(uiState.msg)
+                        UiState.Loading -> showLoading()
+                        UiState.Empty -> showNoDataPlaceholder()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showLoading() {
+        viewBinding.progressBarLayout.isVisible = true
+        viewBinding.errorLayout.isVisible = false
+        viewBinding.noDataLayout.isVisible = false
+    }
+
+    private fun showNoDataPlaceholder() {
+        viewBinding.progressBarLayout.isVisible = false
+        viewBinding.errorLayout.isVisible = false
+        viewBinding.noDataLayout.isVisible = true
+    }
+
+    private fun showError(msg: String) {
+        viewBinding.progressBarLayout.isVisible = false
+        viewBinding.errorLayout.isVisible = true
+        viewBinding.noDataLayout.isVisible = false
+        viewBinding.errorTextView.text = msg
+    }
+
+    private fun showFavouriteGamesList(games: List<Game>) {
+        viewBinding.apply {
+            progressBarLayout.isVisible = false
+            viewBinding.errorLayout.isVisible = false
+            viewBinding.noDataLayout.isVisible = false
+        }
+
+        adapter?.submitList(games)
+    }
 }
---
+++ app/src/main/java/com/example/twitchapp/ui/favourite/FavouriteGamesViewModel.kt
@@ -0,0 +1,40 @@
+package com.example.twitchapp.ui.favourite
+
+import android.content.Context
+import androidx.lifecycle.viewModelScope
+import com.example.twitchapp.R
+import com.example.twitchapp.data.model.Result
+import com.example.twitchapp.data.model.game.Game
+import com.example.twitchapp.data.repository.Repository
+import com.example.twitchapp.ui.BaseViewModel
+import com.example.twitchapp.ui.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
+import java.net.UnknownHostException
+import javax.inject.Inject
+
+@HiltViewModel
+class FavouriteGamesViewModel @Inject constructor(
+    @ApplicationContext context: Context,
+    private val repository: Repository
+) : BaseViewModel(context) {
+
+    val uiState = mutableStateFlow(UiState.Loading as UiState<List<Game>>)
+
+    init {
+        viewModelScope.launch {
+            uiState.setValue(handleResult(Result.Error(UnknownHostException()))) //TODO Get from repo
+        }
+    }
+
+    private fun handleResult(result: Result<List<Game>>): UiState<List<Game>> =
+        when (result) {
+            is Result.Success -> UiState.Loaded(result.data)
+            is Result.Error -> UiState.Error(handleError(result.e))
+            Result.Empty -> UiState.Empty
+        }
+
+    private fun handleError(e: Throwable): String =
+        e.localizedMessage ?: getString(R.string.unknown_error_msg) //TODO
+}
