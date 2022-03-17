d483ae898af4c1bd477390b8cc3e2ef3c7dacee9
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-17 16:57:00 +0700

    Вынес const, и общие поля нотификаций

--- app/src/main/java/com/example/twitchapp/model/notifications/TwitchNotification.kt
+++ app/src/main/java/com/example/twitchapp/model/notifications/TwitchNotification.kt
@@ -1,13 +1,21 @@
 package com.example.twitchapp.model.notifications
 
 sealed class TwitchNotification {
+    abstract val title: String
+    abstract val description: String
+
     data class GameNotification(
+        override val title: String,
+        override val description: String,
         val gameName: String,
         val streamerName: String,
         val viewersCount: Long,
         val date: Long
     ): TwitchNotification()
+
     data class StreamNotification(
+        override val title: String,
+        override val description: String,
         val amount: Int,
         val date: Long
     ): TwitchNotification()
