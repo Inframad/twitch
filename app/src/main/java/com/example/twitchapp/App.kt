47a72b8c0c1df6e8a4a3898f99ba8cab62f509ac
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-17 16:20:22 +0700

    Добавил слушатель состояний приложения, прием уведомлений разного типа

--- app/src/main/java/com/example/twitchapp/App.kt
+++ app/src/main/java/com/example/twitchapp/App.kt
@@ -1,7 +1,47 @@
 package com.example.twitchapp
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App: Application() {
+
+    private var _currentAppState: AppState? = null
+    val currentAppState get() = _currentAppState
+
+    init {
+        registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks {
+            override fun onActivityCreated(p0: Activity, p1: Bundle?) {
+                _currentAppState = AppState.OnActivityCreated
+            }
+
+            override fun onActivityStarted(p0: Activity) {
+                _currentAppState = AppState.OnActivityStarted
+            }
+
+            override fun onActivityResumed(p0: Activity) {
+                _currentAppState = AppState.OnActivityResumed
+            }
+
+            override fun onActivityPaused(p0: Activity) {
+                _currentAppState = AppState.OnActivityPaused
+            }
+
+            override fun onActivityStopped(p0: Activity) {
+                _currentAppState = AppState.OnActivityStopped
+            }
+
+            override fun onActivitySaveInstanceState(p0: Activity, p1: Bundle) {
+                _currentAppState = AppState.OnActivitySaveInstanceState
+            }
+
+            override fun onActivityDestroyed(p0: Activity) {
+                _currentAppState = AppState.OnActivityDestroyed
+            }
+
+        })
+    }
+}
+
--- app/src/main/java/com/example/twitchapp/notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/twitchapp/notification/MyFirebaseMessagingService.kt
@@ -1,6 +1,16 @@
 package com.example.twitchapp.notification
 
 import android.annotation.SuppressLint
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.example.twitchapp.App
+import com.example.twitchapp.AppState
+import com.example.twitchapp.R
 import com.example.twitchapp.model.notifications.TwitchNotification
 import com.example.twitchapp.repository.notification.NotificationRepositoryImpl
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -23,20 +33,69 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         val intent = message.toIntent()
         intent.action = "com.example.twitchapp.message"
-        intent.extras?.let {
+        val notificationModel = intent.extras?.let {
+            createNotificationModel(it)
+        }
+
+        notificationModel?.let {
             CoroutineScope(Dispatchers.IO).launch {
-                notificationRepository.save(
-                    TwitchNotification.GameNotification(
-                        gameName = it.getString("game_name")
-                            ?: throw NullPointerException("gameName from FCM is null"),
-                        streamerName = it.getString("streamer_name")
-                            ?: throw java.lang.NullPointerException("streamerName from FCM is null"),
-                        viewersCount = it.getLong("viewers_count"),
-                        date = 12345678L
-                    )
-                )
+                notificationRepository.save(it)
+            }
+
+            when ((application as App).currentAppState) {
+                AppState.OnActivityCreated,
+                AppState.OnActivityStarted,
+                AppState.OnActivityResumed -> {
+                    applicationContext.sendBroadcast(intent)
+                }
+                else -> { showNotification(it) }
             }
         }
-        applicationContext.sendBroadcast(intent)
+    }
+
+    private fun showNotification(notification: TwitchNotification) {
+        val CHANNEL_ID = "CHANNEL"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "channel"
+            val descriptionText = "desc"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(
+                123, NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_twitch)
+                    .setContentTitle("Test")
+                    .setContentText("textContent")
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT).build()
+            )
+        }
+    }
+
+    private fun createNotificationModel(bundle: Bundle): TwitchNotification {
+        bundle.apply {
+            return when (val type = bundle.getString("notification_type")) {
+                "STREAMS" -> TwitchNotification.StreamNotification(
+                    bundle.getInt("streams_amount"),
+                    123
+                ) //TODO date
+                "GAME" -> TwitchNotification.GameNotification(
+                    gameName = getString("game_name")
+                        ?: throw NullPointerException("gameName from FCM is null"),
+                    streamerName = getString("streamer_name")
+                        ?: throw java.lang.NullPointerException("streamerName from FCM is null"),
+                    viewersCount = getLong("viewers_count"),
+                    date = 12345678L
+                )
+                else -> throw IllegalArgumentException("Unknown notification type: $type")
+            }
+        }
     }
 }
