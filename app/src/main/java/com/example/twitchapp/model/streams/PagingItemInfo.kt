9fa72497bf75d699a883928a44d8ea740be2415d
AlexeyTyukavkin <infinsinity@gmail.com>
2022-03-28 11:55:57 +0700

    Убрал неиспользуемые классы

--- app/src/main/java/com/example/twitchapp/model/streams/PagingItemInfo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.twitchapp.model.streams
-
-enum class PagingItemInfo {
-    NO_INTERNET_LOCAL, NO_INTERNET_EMPTY
-}
--- app/src/main/java/com/example/twitchapp/model/streams/PagingStreamItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.twitchapp.model.streams
-
-class PagingStreamItem(
-    val data: GameStream,
-    val info: PagingItemInfo?
-)
